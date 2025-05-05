0ae55d197ed340185263e74d32c9414e50acab79
gkibria121 <gkibria121@gmail.com>
2025-05-05 11:12:17 +0600

    add bulk product schema

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -4,3 +4,9 @@ export const productSchema = z.object({
   id: z.string().nonempty(),
   name: z.string().nonempty("This field is required"),
 });
+
+export const bulkProductsSchema = z.object({
+  products: z
+    .array(productSchema.omit({ id: true }))
+    .min(1, "At least one product is required"),
+});
